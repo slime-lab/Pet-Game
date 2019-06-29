b26664c5f5445f310ba7800990c261cc7c7cd48e
Marian <maazurin@gmail.com>
2019-06-29 12:28:17 -0700

    added petbutton.js

---
+++ assets/petbuttons.js
@@ -0,0 +1,21 @@
+//pet button clicked --
+//if button is clicked then default image change into happy image
+//after 2 hours with no click, change image back to default
+$(".boxOne").click(function (happyPet) {
+
+    alert('Pet is now happy');
+
+});
+
+//feed button clicked -- food appears
+//if button is clicked then default image change into happy image
+//after 4 hours with no click, change image back to default
+$(".boxTwo").click(function (hungryPet) {
+
+    alert('Pet is not hungry anymore');
+
+});
+
+//angry slime swaps with default image if no button is clicked for 8 hours
+//pet and feed button must be clicked to make the slime happy
+//after 4 hours with no click, change image back to default
