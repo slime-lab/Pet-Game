61749b5d46384c3422df8453356dc5b01d4d5b0f
Marian <maazurin@gmail.com>
2019-06-29 20:26:57 -0700

    working pet buttons

--- assets/petbuttons.js
+++ assets/petbuttons.js
@@ -1,9 +1,14 @@
 //pet button clicked --
 //if button is clicked then default image change into happy image
 //after 2 hours with no click, change image back to default
-$(".boxOne").click(function (happyPet) {
+function defaultMood() {
+    $(".displayPetImg").attr("src", "images/avatar/idleAvatar.gif")
+}
 
-    alert('Pet is now happy');
+$(".boxOne").click(function (happyPet) {
+    $(".petCurrentMood").html("Pet is happy!")
+    $(".displayPetImg").attr("src", "images/avatar/happyAvatar.gif");
+    setTimeout(defaultMood, 5000);
 
 });
 
@@ -11,11 +16,30 @@ $(".boxOne").click(function (happyPet) {
 //if button is clicked then default image change into happy image
 //after 4 hours with no click, change image back to default
 $(".boxTwo").click(function (hungryPet) {
-
-    alert('Pet is not hungry anymore');
+    $(".petCurrentMood").html("Pet is full!")
+    $(".displayPetImg").attr("src", "images/avatar/happyAvatar.gif");
+    setTimeout(defaultMood, 5000);
 
 });
 
-//angry slime swaps with default image if no button is clicked for 8 hours
-//pet and feed button must be clicked to make the slime happy
-//after 4 hours with no click, change image back to default
+//detect if buttons are clicked
+$('.boxOne').on('click', function () {
+    $(this).data('clicked', 'yes');
+    console.log("i clicked it!");
+});
+
+//when you click on feed, the meat image shows up 
+$('.boxTwo').on('click', function () {
+    $(this).data('clicked', 'yes');
+    console.log("i clicked it!");
+    $(".displayFoodImg").attr("src", "images/food/meat.png");
+    setTimeout(defaultMood, 5000);
+
+});
+
+//if button hasnt been clicked for set number of hours, avatar becomes upset
+var isClicked = $('.boxOne').data('clicked');
+    if (isClicked != 'yes') {
+        $(".displayPetImg").attr("src", "images/avatar/upsetAvatar.gif");
+    } else (defaultMood);
+
