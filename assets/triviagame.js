940251282d2eb2f992bd64e83ac545f4bb7f6ed4
samy takkish <takkish_23@hotmail.com>
2019-07-01 10:13:08 -0700

    removed loop for correct function

--- assets/triviagame.js
+++ assets/triviagame.js
@@ -17,75 +17,76 @@ $("#quiz-button").on("click", function () {
     })
         .then(function (response) {       // After the data from the AJAX request comes back
             // for loop through the questions
-            // console.log(response)
-            for (var i = 0; i < response.results.length; i++) {
-                var questionUrl = response.results[i].question;
-                var correct = response.results[i].correct_answer;
-                var incorrect = response.results[i].incorrect_answers;
-                console.log(correct);
-                console.log(incorrect);
+            console.log(response)
+            //removing for loop for correct function
+            var questionUrl = response.results[0].question;
+            var correct = response.results[0].correct_answer;
+            var incorrect = response.results[0].incorrect_answers;
+            console.log(correct);
+            console.log(incorrect);
 
-                $("#body").html(questionUrl)
-                var quizquestion = $("<div>");
-                var truebtn = $("<button>").text("True");
-                var falsebtn = $("<button>").text("False");
-                //changing gifs and images
-                $("#body").append("<br><br>");
-                $("#body").append(truebtn);
-                $("#body").append(falsebtn);
-                //swap to default pet face function
-                function defaultEmoji() {
-                    $(".pet").attr("src", "images/avatar/newidleAvatar.gif")
-                }
-                //correct faceswap function
-                function correctPetFace() {
-                    points++;
-                    $(".points").html("Corrent Answers: " + points);
-                    $(".pet").attr("src", "images/avatar/happyAvatar.gif")
-                    setTimeout(defaultEmoji, 5000);
+            $("#body").html(questionUrl)
+            var quizquestion = $("<div>");
+            var truebtn = $("<button>").text("True");
+            var falsebtn = $("<button>").text("False");
+            //changing gifs and images
+            $("#body").append("<br><br>");
+            $("#body").append(truebtn);
+            $("#body").append(falsebtn);
+            //swap to default pet face function
+            function defaultEmoji() {
+                $(".pet").attr("src", "images/avatar/newidleAvatar.gif")
+            }
+            //correct faceswap function
+            function correctPetFace() {
+                points++;
+                $(".points").html("Corrent Answers: " + points);
+                $(".pet").attr("src", "images/avatar/happyAvatar.gif")
+                setTimeout(defaultEmoji, 5000);
 
-                }
-                //incorrect faceswap function
-                function incorrectPetFace() {
-                    $(".pet").attr("src", "images/avatar/xAvatar.gif")
-                    setTimeout(defaultEmoji, 5000);
-                }
-                //answer click function
-                truebtn.on("click", function (event) {
-                    //changing alert to text
-                    var x = "True";
-                    console.log(questionAnswered)
-                    if (questionAnswered == false) {
-                        questionAnswered = true;
-                        if (x == correct) {
-                            console.log(x)
-                            $("#correctAns").text("hey you got it!");
-                            correctPetFace();
-
-                        } else {
-                            $("#correctAns").text("sorry the correct answer is:" + correct);
-                            incorrectPetFace();
-                        }
-                    }
-                });
+            }
+            //incorrect faceswap function
+            function incorrectPetFace() {
+                $(".pet").attr("src", "images/avatar/xAvatar.gif")
+                setTimeout(defaultEmoji, 5000);
+            }
+            //answer click function
+            truebtn.on("click", function (event) {
                 //changing alert to text
-                falsebtn.on("click", function (event) {
-                    if (questionAnswered == false) {
+                var x = "True";
+                console.log(questionAnswered)
+                if (questionAnswered == false) {
+                    questionAnswered = true;
+                    if (x == correct) {
+                        console.log(x)
+                        $("#correctAns").text("hey you got it!");
+                        correctPetFace();
+                        //happy ++
 
-                        questionAnswered = true;
-                        var y = "False";
-                        if (y == correct) {
-                            console.log(questionAnswered)
-                            $("#correctAns").text("hey you got it!");
-                            correctPetFace();
-
-                        } else {
-                            $("#correctAns").text("sorry the correct answer is:" + correct);
-                            incorrectPetFace();
-                        }
+                    } else {
+                        $("#correctAns").text("sorry the correct answer is:" + correct);
+                        incorrectPetFace();
                     }
+                }
+            });
+            //changing alert to text
+            falsebtn.on("click", function (event) {
+                if (questionAnswered == false) {
 
-                });
-            };
+                    questionAnswered = true;
+                    var y = "False";
+                    if (y == correct) {
+                        console.log(questionAnswered)
+                        $("#correctAns").text("hey you got it!");
+                        correctPetFace();
+
+                    } else {
+                        $("#correctAns").text("sorry the correct answer is:" + correct);
+                        incorrectPetFace();
+                    }
+                }
+
+            });
+
         });
 });
