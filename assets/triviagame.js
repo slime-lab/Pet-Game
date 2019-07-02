7f2fb0836d0e9c7c2d53a3e6b6aa2925c416d112
Michael Eguina <michael@corner-exit.com>
2019-07-02 10:43:49 -0700

    trivia questions work

--- assets/triviagame.js
+++ assets/triviagame.js
@@ -1,13 +1,15 @@
 //point counter
 var points = 0;
+var questionTimer;
 //quiz toggle
-var questionAnswered = false;
 // Event listener for our quiz-button
 $(".boxThree").on("click", newQuestion());
 
-function newQuestion()
-{
-    questionAnswered = false;
+function newQuestion() {
+    clearTimeout(questionTimer)
+    var questionAnswered = false;
+
+
     //adding .empty funtion for quiz
     $("#correctAns").empty();
 
@@ -20,69 +22,69 @@ function newQuestion()
     })
         .then(function (response) {       // After the data from the AJAX request comes back
             // for loop through the questions
-            console.log(response)
             //removing for loop for correct function
             var questionUrl = response.results[0].question;
             var correct = response.results[0].correct_answer;
             var incorrect = response.results[0].incorrect_answers;
-            console.log(correct);
-            console.log(incorrect);
+            console.log(correct)
 
-                $(".questions").html(questionUrl)
-                // var quizquestion = $("<div>");
-                // var truebtn = $("<button>").text("True");
-                // var falsebtn = $("<button>").text("False");
-                var truebtn = $(".truebtn");
-                var falsebtn = $(".falsebtn");
+            $(".questions").html(questionUrl)
+            // var quizquestion = $("<div>");
+            // var truebtn = $("<button>").text("True");
+            // var falsebtn = $("<button>").text("False");
+            var truebtn = $(".truebtn");
+            var falsebtn = $(".falsebtn");
 
-                // $(".quizBox").append("<br><br>");
-                // $(".quizBox").append(truebtn);
-                // $(".quizBox").append(falsebtn);
-                //swap to default pet face function
-                function defaultEmoji() {
-                    $(".displayPetImg").attr("src", "images/avatar/idleAvatar.gif")
-                }
-                //correct faceswap function
-                function correctPetFace() {
-                    points++;
-                    $(".points").html("Corrent Answers: " + points);
-                    $(".displayPetImg").attr("src", "images/avatar/happyAvatar.gif")
-                    setTimeout(defaultEmoji, 5000);
-                    setTimeout(newQuestion, 2000);
+            // $(".quizBox").append("<br><br>");
+            // $(".quizBox").append(truebtn);
+            // $(".quizBox").append(falsebtn);
+            //swap to default pet face function
+            function defaultEmoji() {
+                $(".displayPetImg").attr("src", "images/avatar/idleAvatar.gif")
+            }
+            //correct faceswap function
+            function correctPetFace() {
+                points++;
+                $(".points").html("Corrent Answers: " + points);
+                $(".displayPetImg").attr("src", "images/avatar/happyAvatar.gif")
+                setTimeout(defaultEmoji, 2000);
+                questionTimer = setTimeout(newQuestion, 2000);
 
-                }
-                //incorrect faceswap function
-                function incorrectPetFace() {
-                    $(".displayPetImg").attr("src", "images/avatar/xAvatar.gif")
-                    setTimeout(defaultEmoji, 5000);
-                    setTimeout(newQuestion, 2000);
-                }
-                //answer click function
-                truebtn.on("click", function (event) {
+
+
+            }
+            //incorrect faceswap function
+            function incorrectPetFace() {
+                $(".displayPetImg").attr("src", "images/avatar/xAvatar.gif")
+                setTimeout(defaultEmoji, 2000);
+                questionTimer = setTimeout(newQuestion, 2000);
+
+            }
+            //answer click function
+            truebtn.one("click", function (event) {
+                if (questionAnswered == false) {
                     //changing alert to text
                     var x = "True";
-                    console.log(questionAnswered)
-                    if (questionAnswered == false) {
-                        questionAnswered = true;
-                        if (x == correct) {
-                            console.log(x)
-                            $("#correctAns").text("hey you got it!");
-                            correctPetFace();
+
+                    questionAnswered = true;
+                    if (x == correct) {
+                        $("#correctAns").text("hey you got it!");
+                        correctPetFace();
 
                     } else {
                         $("#correctAns").text("sorry the correct answer is:" + correct);
                         incorrectPetFace();
+
                     }
                 }
             });
             //changing alert to text
-            falsebtn.on("click", function (event) {
+            falsebtn.one("click", function (event) {
                 if (questionAnswered == false) {
 
                     questionAnswered = true;
                     var y = "False";
                     if (y == correct) {
-                        console.log(questionAnswered)
                         $("#correctAns").text("hey you got it!");
                         correctPetFace();
 
