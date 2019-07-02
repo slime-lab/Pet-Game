f7b1617e568330ef0b418c1bcd162878b1adf05b
Michael Eguina <michael@corner-exit.com>
2019-07-02 12:48:12 -0700

    localstorage hunger

--- assets/petbuttons.js
+++ assets/petbuttons.js
@@ -1,13 +1,28 @@
 //pet button clicked --
 //if button is clicked then default image change into happy image
 //after 2 hours with no click, change image back to default
+var currentTime = moment().format("X");
+function timeNow (){
+    currentTime = moment().format("X");
+}
+console.log(localStorage.feedtime);
+$(".footer").html("Pet was last fed at " + localStorage.feedtime)
+console.log( currentTime-localStorage.feedtimeUnix)
+function checkHungry(){
+    if (currentTime -localStorage.feedtimeUnix >7200 ){
+    $(".petCurrentMood").text("Pet is Hungry");
+    console.log("test")
+}}
+setInterval(checkHungry,2000)
+setInterval(timeNow,1000)
+
 function defaultMood() {
     $(".displayPetImg").attr("src", "images/avatar/idleAvatar.gif");
 }
 
 function defaultFood() {
     $(".displayFoodImg").attr("src", "");
-} 
+}
 
 $(".boxOne").click(function (happyPet) {
     $(".petCurrentMood").html("Pet is happy!")
@@ -20,12 +35,16 @@ $(".boxOne").click(function (happyPet) {
 //if button is clicked then default image change into happy image
 //after 4 hours with no click, change image back to default
 $(".boxTwo").click(function (hungryPet) {
-    var foods = ["images/food/apple.png", "images/food/icecream.png" , "images/food/meat.png"];
-var randfoods = foods[Math.floor(Math.random() * foods.length)];
+    var foods = ["images/food/apple.png", "images/food/icecream.png", "images/food/meat.png"];
+    var randfoods = foods[Math.floor(Math.random() * foods.length)];
     $(".petCurrentMood").html("Pet is full!")
     $(".displayFoodImg").attr("src", randfoods);
     setTimeout(defaultFood, 2000);
     $(".displayPetImg").attr("src", "images/avatar/fullAvatar.gif");
     setTimeout(defaultMood, 5000);
+    var format1 = moment().format("LTS");
+    localStorage.feedtimeUnix = currentTime;
+    localStorage.feedtime = format1;
+    $(".footer").text("Pet was last fed at " + format1)
 
 });
