eb1bb2dc268221dd6f41dc3148d93336c9931e29
Marian <maazurin@gmail.com>
2019-07-02 13:35:07 -0700

    added local storage for when a user gives a pet name

--- assets/timekeeper.js
+++ assets/timekeeper.js
@@ -1,22 +1,30 @@
+var petNames = localStorage.getItem(petNames);
+if (!petNames) {
+    petNames = prompt('Give your pet a name: ')
+    localStorage.setItem(petNames, "petname");
+    console.log(petNames);
+
+};
+
 function timekeeper() {
     var currentTime = moment().format("X");
-    function timeNow (){
+    function timeNow() {
         currentTime = moment().format("X");
     }
     console.log(localStorage.feedtime);
-    $(".petCurrentMood").html("Pet was last fed at " + localStorage.feedtime)
-    console.log( currentTime-localStorage.feedtimeUnix)
-    function checkHungry(){
-        if (currentTime -localStorage.feedtimeUnix >1 ){
-        $(".isHungry").text("Pet is Hungry");
-        console.log("test")
-    }}
-setInterval(checkHungry,2000)
-setInterval(timeNow,1000)
-      
-      //document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    $(".petCurrentMood").html(petNames + " was last fed at " + localStorage.feedtime)
+    console.log(currentTime - localStorage.feedtimeUnix)
+    function checkHungry() {
+        if (currentTime - localStorage.feedtimeUnix > 1) {
+            $(".isHungry").text(petNames + " is Hungry");
+        }
+    }
+    setInterval(checkHungry, 2000)
+    setInterval(timeNow, 1000)
 
-    
+    //document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+
+
 
     //feed button clicked
     //if button is clicked then default image change into happy image
@@ -26,9 +34,18 @@ setInterval(timeNow,1000)
         localStorage.feedtimeUnix = currentTime;
         localStorage.feedtime = format1;
 
-        $(".petCurrentMood").html("Pet was last fed at " + format1)
-        $(".isHungry").text("Pet is Full");
-       
+        $(".petCurrentMood").html(petNames + " was last fed at " + format1)
+        $(".isHungry").text(petNames + " is Full");
+
 
     });
+
+    // var name = [];
+    // $(function storeNames(form) {
+
+
+
 }
+
+
+
