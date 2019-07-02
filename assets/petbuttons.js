03afc574322255b97be807701575b85e3bc63f85
Marian <maazurin@gmail.com>
2019-07-02 13:47:35 -0700

    ask user for pet name and save to local storage

--- assets/petbuttons.js
+++ assets/petbuttons.js
@@ -1,3 +1,13 @@
+//ask user for pet name and save to local storage
+
+var petName = localStorage.getItem(petName);
+if (!petName) {
+    petName = prompt('Give your pet a name: ')
+    localStorage.setItem(petName, "petname");
+    console.log(petName);
+
+};
+
 //pet button clicked --
 //if button is clicked then default image change into happy image
 //after 2 hours with no click, change image back to default
@@ -6,11 +16,11 @@ function timeNow (){
     currentTime = moment().format("X");
 }
 console.log(localStorage.feedtime);
-$(".footer").html("Pet was last fed at " + localStorage.feedtime)
+$(".footer").html(petName + " was last fed at " + localStorage.feedtime)
 console.log( currentTime-localStorage.feedtimeUnix)
 function checkHungry(){
     if (currentTime -localStorage.feedtimeUnix >7200 ){
-    $(".petCurrentMood").text("Pet is Hungry");
+    $(".petCurrentMood").text(petName + " is Hungry");
     console.log("test")
 }}
 setInterval(checkHungry,2000)
@@ -25,7 +35,7 @@ function defaultFood() {
 }
 
 $(".boxOne").click(function (happyPet) {
-    $(".petCurrentMood").html("Pet is happy!")
+    $(".petCurrentMood").html(petName + " is happy!")
     $(".displayPetImg").attr("src", "images/avatar/happyAvatar.gif");
     setTimeout(defaultMood, 5000);
 
@@ -37,7 +47,7 @@ $(".boxOne").click(function (happyPet) {
 $(".boxTwo").click(function (hungryPet) {
     var foods = ["images/food/apple.png", "images/food/icecream.png", "images/food/meat.png"];
     var randfoods = foods[Math.floor(Math.random() * foods.length)];
-    $(".petCurrentMood").html("Pet is full!")
+    $(".petCurrentMood").html(petName + " is full!")
     $(".displayFoodImg").attr("src", randfoods);
     setTimeout(defaultFood, 2000);
     $(".displayPetImg").attr("src", "images/avatar/fullAvatar.gif");
@@ -45,6 +55,6 @@ $(".boxTwo").click(function (hungryPet) {
     var format1 = moment().format("LTS");
     localStorage.feedtimeUnix = currentTime;
     localStorage.feedtime = format1;
-    $(".footer").text("Pet was last fed at " + format1)
+    $(".footer").text(petName + " was last fed at " + format1)
 
 });
